06d8ba3a517a5af226dc2560470ab8dce1a26cbb
GeorgiStanev92 <georgistanev92@gmail.com>
2022-04-02 14:26:26 +0300

    added ILaptopService and extended LaptopService

---
+++ PCStore.Core/Contracts/ILaptopService.cs
@@ -0,0 +1,12 @@
+﻿using PCStore.Infrastrucure.Data;
+
+namespace PCStore.Core.Contracts
+{
+    public interface ILaptopService
+    {
+        IQueryable<Laptop> GetAll();
+        void Add(Laptop laptop);
+        void Delete(Laptop laptop);
+        void Update(Laptop laptop);
+    }
+}
---
+++ PCStore.Core/Services/LaptopService.cs
@@ -0,0 +1,65 @@
+﻿using PCStore.Core.Contracts;
+using PCStore.Infrastrucure.Data;
+using PCStore.Infrastrucure.Repositories;
+
+namespace PCStore.Core.Services
+{
+    public class LaptopService : ILaptopService
+    {
+        private readonly IApplicatioDbRepository repo;
+
+        public LaptopService(IApplicatioDbRepository _repo)
+        {
+            repo = _repo;
+        }
+
+        public IQueryable<Laptop> GetAll()
+        {
+            return repo.All<Laptop>();
+        }
+
+        public void Update(Laptop laptop)
+        {
+            repo.Update(laptop);
+        }
+
+        public async void Add(Laptop laptop)
+        {
+           repo.AddAsync(laptop);
+        }
+
+        public void Delete(Laptop laptop)
+        {
+            repo.Delete(laptop);
+        }
+
+        //public async Task PlaceOrder(CustomerOrder order)
+        //{
+        //    var customerNumber = order.CustomerNumber.Trim();
+        //    var customer = await repo.All<Contragent>()
+        //        .FirstOrDefaultAsync(c => c.CustomerNumber == customerNumber);
+
+        //    if (customer == null)
+        //    {
+        //        throw new ArgumentException("Customer doesn`t exist");
+        //    }
+
+        //    var barcode = order.Computers
+        //        .Select(c => c.Barcode)
+        //        .ToArray();
+
+        //    var laptops = await repo.All<Laptop>()
+        //        .Where(i => barcode.Contains(i.Barcode))
+        //        .ToDictionaryAsync(c => c.Barcode, c => c.ItemsCount);
+
+        //    foreach (var laptop in order.Computers)
+        //    {
+        //        if (!laptops.ContainsKey(laptop.Barcode) ||
+        //            laptop.Count > laptops[laptop.Barcode])
+        //        {
+        //            throw new ArgumentException("Not enough quantity");
+        //        }
+        //    }
+        //}
+    }
+}
