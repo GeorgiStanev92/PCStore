54d99a4d2c4af2f79bc5e42b94ff188f9503c737
GeorgiStanev92 <georgistanev92@gmail.com>
2022-04-02 14:20:24 +0300

    Extended display service

---
+++ PCStore.Core/Contracts/IDisplayService.cs
@@ -0,0 +1,12 @@
+﻿using PCStore.Infrastrucure.Data;
+
+namespace PCStore.Core.Contracts
+{
+    public interface IDisplayService
+    {
+        IQueryable<Display> GetAll();
+        void Add(Display display);
+        void Delete(Display display);
+        void Update(Display display);
+    }
+}
---
+++ PCStore.Core/Services/DisplayService.cs
@@ -0,0 +1,65 @@
+﻿using PCStore.Core.Contracts;
+using PCStore.Infrastrucure.Data;
+using PCStore.Infrastrucure.Repositories;
+
+namespace PCStore.Core.Services
+{
+    public class DisplayService : IDisplayService
+    {
+        private readonly IApplicatioDbRepository repo;
+
+        public DisplayService(IApplicatioDbRepository _repo)
+        {
+            repo = _repo;
+        }
+
+        public IQueryable<Display> GetAll()
+        {
+            return repo.All<Display>();
+        }
+
+        public void Update(Display display)
+        {
+            repo.Update(display);
+        }
+
+        public async void Add(Display display)
+        {
+          await repo.AddAsync(display);
+        }
+
+        public void Delete(Display display)
+        {
+            repo?.Delete(display);
+        }
+
+        //public async Task PlaceOrder(CustomerOrder order)
+        //{
+        //    var customerNumber = order.CustomerNumber.Trim();
+        //    var customer = await repo.All<Contragent>()
+        //        .FirstOrDefaultAsync(c => c.CustomerNumber == customerNumber);
+
+        //    if (customer == null)
+        //    {
+        //        throw new ArgumentException("Customer doesn`t exist");
+        //    }
+
+        //    var barcode = order.Computers
+        //        .Select(c => c.Barcode)
+        //        .ToArray();
+
+        //    var displays = await repo.All<Display>()
+        //        .Where(i => barcode.Contains(i.Barcode))
+        //        .ToDictionaryAsync(c => c.Barcode, c => c.ItemsCount);
+
+        //    foreach (var display in order.Computers)
+        //    {
+        //        if (!displays.ContainsKey(display.Barcode) ||
+        //            display.Count > displays[display.Barcode])
+        //        {
+        //            throw new ArgumentException("Not enough quantity");
+        //        }
+        //    }
+        //}
+    }
+}
