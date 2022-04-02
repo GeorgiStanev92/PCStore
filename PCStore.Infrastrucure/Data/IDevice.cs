e720206dda4edf0144e1cb1d0419c02da9875b33
GeorgiStanev92 <georgistanev92@gmail.com>
2022-04-02 15:19:15 +0300

    User table

---
+++ PCStore.Infrastrucure/Data/IDevice.cs
@@ -0,0 +1,10 @@
+﻿namespace PCStore.Infrastrucure.Data
+{
+    public interface IDevice
+    {
+        decimal Price { get; set; }
+        string SellerPhone { get; set; }
+        User Seller { get; set; }
+        string Description { get; set; }
+    }
+}
---
+++ PCStore.Infrastrucure/Data/User.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace PCStore.Infrastrucure.Data
+{
+    public class User : IdentityUser
+    {
+        private ICollection<IDevice> devices;
+
+        public User()
+        {
+            devices = new HashSet<IDevice>();
+        }
+
+        public bool IsDeleted { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? DeletedOn { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? CreatedOn { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? ModifiedOn { get; set; }
+
+        public virtual ICollection<IDevice> Devices 
+        {
+            get
+            {
+                return devices;
+            }
+            set
+            {
+                devices = value;
+            }
+        }
+    }
+}
