5e8ab9e92cfeb42e36f7aaaa699e1bb04ed779d9
GeorgiStanev92 <georgistanev92@gmail.com>
2022-04-05 20:33:52 +0300

    User service added

---
+++ PCStore.Core/Contracts/IUsersService.cs
@@ -0,0 +1,11 @@
+﻿using PCStore.Infrastrucure.Data;
+
+namespace PCStore.Core.Contracts
+{
+    public interface IUsersService
+    {
+        IQueryable<User> GetAll();
+        void Delete(User user);
+        void Update(User user);
+    }
+}
---
+++ PCStore.Core/Services/UsersService.cs
@@ -0,0 +1,31 @@
+﻿using PCStore.Core.Contracts;
+using PCStore.Infrastrucure.Data;
+using PCStore.Infrastrucure.Repositories;
+
+namespace PCStore.Core.Services
+{
+    public class UsersService : IUsersService
+    {
+        private readonly IApplicatioDbRepository repo;
+
+        public UsersService(IApplicatioDbRepository _repo)
+        {
+            repo = _repo;
+        }
+
+        public IQueryable<User> GetAll()
+        {
+            return repo.All<User>();
+        }
+
+        public void Update(User user)
+        {
+            repo.Update(user);
+        }
+
+        public void Delete(User user)
+        {
+            this.repo.Delete(user);
+        }
+    }
+}
